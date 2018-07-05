2dced1e51759af82ee5b7cba35556f50a0f5e005
CNBlackJ <yes.heng@icloud.com>
2018-07-05 21:44:42 +0800

    chore: update deploy script

--- deploy.sh
+++ deploy.sh
@@ -11,7 +11,8 @@ git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io
-git push -f https://6b21bd448da63398298896a191f5d9408ae42f63@github.com/CNBlackJ/cnblackj.github.io.git master
+# git push -f https://6b21bd448da63398298896a191f5d9408ae42f63@github.com/CNBlackJ/cnblackj.github.io.git master
+git push -f git@github.com:CNBlackJ/cnblackj.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
